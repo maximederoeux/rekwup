6f4fdb63b8bbc308def58ab9a2cf966aaa7bbe0a
maximederoeux <mderoeux@yahoo.com>
2015-12-23 19:09:10 +0100

    Translated Users Sign up and in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-#  scope "/:locale" do
-  resources :users
+
+	devise_for :users 
+	resources :users
 	root to: 'users#index'
-	devise_for :users
-	  
-#	end
+	
+	 
 end
